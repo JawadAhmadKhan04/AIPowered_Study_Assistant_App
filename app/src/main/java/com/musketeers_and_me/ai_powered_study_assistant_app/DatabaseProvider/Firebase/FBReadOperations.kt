ffca2753793e9079ee5df3a3bdc50acf3cbb6d74
JawadAhmadKhan04 <114512817+JawadAhmadKhan04@users.noreply.github.com>
2025-04-21 17:00:49 +0500

    Structure of Packages

---
+++ app/src/main/java/com/musketeers_and_me/ai_powered_study_assistant_app/DatabaseProvider/Firebase/FBReadOperations.kt
@@ -0,0 +1,4 @@
+package com.musketeers_and_me.ai_powered_study_assistant_app.DatabaseProvider.Firebase
+
+class FBReadOperations {
+}
---
+++ app/src/main/java/com/musketeers_and_me/ai_powered_study_assistant_app/Utils.kt
@@ -0,0 +1,5 @@
+package com.musketeers_and_me.ai_powered_study_assistant_app
+
+object Utils {
+
+}
