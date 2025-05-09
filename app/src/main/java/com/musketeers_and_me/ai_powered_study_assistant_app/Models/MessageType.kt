0f2c81840e5e95addeb5a51e06a4edb8927e9948
Abeer Noor <abeer.noor901@gmail.com>
2025-05-10 00:11:07 +0500

    New Files

---
+++ app/src/main/java/com/musketeers_and_me/ai_powered_study_assistant_app/Models/MessageType.kt
@@ -0,0 +1,7 @@
+package com.musketeers_and_me.ai_powered_study_assistant_app.Models
+
+enum class MessageType {
+    REGULAR,  // Regular chat message
+    NOTE,     // Note message
+    TOPIC     // Topic message with topic name
+} 
